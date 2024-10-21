3fce07657cb79db7987d7baab804b0478a9660cf
spappa3k <pappalardo.dev@gmail.com>
2024-10-21 19:10:12 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { CardDetailsComponent } from './card-details/card-details.component';
 
 const routes: Routes = [
   {
-    path: "", component:HomeComponent
+    path: "", component:CardDetailsComponent
   },
   {
     path:"list/:searchedName", component: ListComponent
