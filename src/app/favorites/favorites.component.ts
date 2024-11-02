1bb29b4b377010806ee0111d78bd3f4c9a6d18ed
spappa3k <pappalardo.dev@gmail.com>
2024-11-02 11:53:19 +0100

    .

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { YugiohService } from '../yugioh.service';
+import { Card, CardData } from '../../models/models';
 
 @Component({
-  selector: 'app-favorites',
+  selector: 'app-list',
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css'
 })
-export class FavoritesComponent {
+export class FavoritesComponent implements OnInit{
+nameSearched?:string
+dataSearched?:CardData
+cards?:CardData[]
+cardsMultiplier:number=0
+numberListened:number=20
+numberOfObjectsInCall:number=0
 
-}
+constructor(private route:ActivatedRoute, private ys:YugiohService){}
+
+
+  ngOnInit(): void {
+    this.cards = this.ys.favorites;
+
+    this.cardsMultiplier=0;
+this.numberOfObjectsInCall=0;
+      }
+  }
+
