4e8a7c3d2cf24160f53397f3622757286f0dcc39
spappa3k <pappalardo.dev@gmail.com>
2024-11-14 18:38:43 +0100

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 import { CardDetailsComponent } from './card-details/card-details.component';
 import { FavoritesComponent } from './favorites/favorites.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -17,7 +18,11 @@ const routes: Routes = [
   },
   {
     path:"favorites", component:FavoritesComponent
-  }
+  },
+  {
+    path:"login", component:LoginComponent
+  },
+
 ];
 
 @NgModule({
