c1ce39bd82d781c1129c4b8f9d8d9e50673ea834
spappa3k <pappalardo.dev@gmail.com>
2024-11-21 15:51:08 +0100

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,24 @@ import { ListComponent } from './list/list.component';
 import { CardDetailsComponent } from './card-details/card-details.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { LoginComponent } from './login/login.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: "", component:HomeComponent
   },
   {
-    path:"list/:searchedName", component: ListComponent
+    path:"list/:searchedName", component: ListComponent , canActivate: [authGuard]
   },
   {
-    path:"card/:id", component:CardDetailsComponent
+    path:"card/:id", component:CardDetailsComponent, canActivate: [authGuard]
   },
   {
-    path:"favorites", component:FavoritesComponent
+    path:"favorites", component:FavoritesComponent, canActivate: [authGuard]
   },
-  {
-    path:"login", component:LoginComponent
-  },
+  /*{
+    path:"login", component:LoginComponent, canActivate: [authGuard]
+  },*/
 
 ];
 
